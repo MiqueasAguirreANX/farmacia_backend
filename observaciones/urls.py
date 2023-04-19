f630b44ec3322a758c35d43ce52c881df3470002
MiqueasAguirreANX <70060960+MiqueasAguirreANX@users.noreply.github.com>
2023-04-19 11:45:45 -0300

    new changes

--- observaciones/urls.py
+++ observaciones/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from observaciones import viewsets
+from observaciones.views import GetProtocoloByObservacionWord
 
 router = DefaultRouter()
 router.register(r"observaciones", viewsets.ObservacionViewSet, basename="observaciones")
 
 urlpatterns = [
+    path(
+        "get-protocolo-by-observacion-word",
+        GetProtocoloByObservacionWord.as_view(),
+        name="get-protocolo-by-observacion-word"
+    ),
     path('', include(router.urls)),
 ]
--- observaciones/views.py
+++ observaciones/views.py
@@ -1,2 +1,38 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from clientes.serializers import ClienteSerializer
+from colaboradores.serializers import ColaboradorSerializer
+from observaciones.serializers import ObservacionSerializer
+
+from protocolos.models import Protocolo
+from protocolos.serializers import ProtocoloSerializer
+from servicios.serializers import ServicioSerializer
 
 
+class GetProtocoloByObservacionWord(APIView):
+
+    def post(self, request, *args, **kwargs):
+        word = request.data.get("word")
+        protocolos = Protocolo.objects.filter(observaciones__detalle__contains=word).distinct()
+        data = []
+
+        for protocolo in protocolos:
+            prot = ProtocoloSerializer(protocolo).data
+            observaciones = ObservacionSerializer(protocolo.observaciones, many=True).data
+            cliente = ClienteSerializer(protocolo.cliente).data
+            colaborador = ColaboradorSerializer(protocolo.colaborador).data
+            servicio = ServicioSerializer(protocolo.servicio).data
+
+            data.append({
+                "protocolo": prot,
+                "observaciones": observaciones,
+                "cliente": cliente,
+                "colaborador": colaborador,
+                "servicio": servicio,
+            })
+        
+        return Response(data={
+            "word": word,
+            "data": data,
+        }, status=status.HTTP_200_OK)
